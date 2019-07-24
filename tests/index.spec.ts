f96a9912a2a5d07144a85bf25f5874c72e1f19cf
Evan Coulson <evan.m.coulson@gmail.com>
2019-07-23 18:23:25 -0700

    refactored logger from a constant to a function and removed morgan dependency

---
+++ tests/index.spec.ts
@@ -0,0 +1,21 @@
+test.todo("Creates a logger in dev");
+
+test.todo("Creates a logger in debug");
+
+test.todo("Creates a logger in prod");
+
+test.todo("Fails to create a logger when no file name is passed");
+
+test.todo("Logs to file A");
+
+test.todo("Logs to file B");
+
+test.todo("Logs to console");
+
+test.todo("Logs to info");
+
+test.todo("Logs to debug");
+
+test.todo("Logs to error");
+
+test.todo("Logs to warn");
